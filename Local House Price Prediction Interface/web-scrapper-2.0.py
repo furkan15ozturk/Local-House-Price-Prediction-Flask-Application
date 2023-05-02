6322c7d26356192863a765197989ba0aea01c5f2
Furkan Öztürk <58707213+furkan15ozturk@users.noreply.github.com>
2023-05-02 09:01:36 +0300

    Project is being updated to the point that is all automated. Each house page is opened and the data is collected from the page.
    Error: balcony value gives error and bathroom value seems to be empty

--- Local House Price Prediction Interface/web-scrapper-2.0.py
+++ Local House Price Prediction Interface/web-scrapper-2.0.py
@@ -29,25 +29,52 @@ def update_houses():
                 print(house_href)
                 browser2.get(house_href)
                 house_info = browser2.find_elements(by=By.CSS_SELECTOR, value="._35T4WV")
-                if count == 0:
-                    print(house_info[3].text)
-                    count += 1
-                # 7 -
-                elements = c.find_elements(by=By.CSS_SELECTOR, value="._2UELHn")
-                price = c.find_elements(by=By.CSS_SELECTOR, value="._2C5UCT")
-                location = c.find_elements(by=By.CSS_SELECTOR, value="._2wVG12")
-                try:
-                    for element in elements:
-                        find_m2 = element.find_element(by=By.XPATH, value=".//span[contains(text(),'m2')]")
-                        m2_text = find_m2.text
-                        m2_value = re.search(r'\d+\s*m2', m2_text)
-                        m2 = m2_value.group()
-                        house_list.append({'price': int(price[0].text.replace('\n', '').replace('.', '').replace('TL', '')),
-                                           'location': location[0].text.replace('\n', ''),
-                                           'm2': m2.replace(' m2', '')})
 
-                except:
-                    pass
+                # area_xpath = '//div[contains(text(), "Brüt Metrekare")]/following-sibling::div'
+                # room_xpath = '//div[contains(text(), "Oda Sayısı")]/following-sibling::div'
+                # bathroom_xpath = '//div[contains(text(), "Banyo Sayısı")]/following-sibling::div'
+                # floor_count_xpath = '//div[contains(text(), "Binanın Kat Sayısı")]/following-sibling::div'
+                # balcony_xpath = '//div[contains(text(), "Balkon Sayısı")]/following-sibling::div'
+                area_xpath = '//div[contains(text(), "Brüt Metrekare")]/following-sibling::div[1]'
+                room_xpath = '//div[contains(text(), "Oda Sayısı")]/following-sibling::div[1]'
+                bathroom_xpath = '//div[contains(text(), "Banyo Sayısı")]/following-sibling::div[1]'
+                floor_count_xpath = '//div[contains(text(), "Binanın Kat Sayısı")]/following-sibling::div[1]'
+                balcony_xpath = '//div[contains(text(), "Balkon Sayısı")]/following-sibling::div[1]'
+                area_elements = browser2.find_elements(by=By.XPATH, value=area_xpath)
+                room_elements = browser2.find_elements(by=By.XPATH, value=room_xpath)
+                bathroom_elements = browser2.find_elements(by=By.XPATH, value=bathroom_xpath)
+                floor_count_elements = browser2.find_elements(by=By.XPATH, value=floor_count_xpath)
+                balcony_elements = browser2.find_elements(by=By.XPATH, value=balcony_xpath)
+
+                area_value = area_elements[0].text if area_elements else None
+                room_value = room_elements[0].text if room_elements else None
+                bathroom_value = bathroom_elements[0].text if bathroom_elements else None
+                floor_count_value = floor_count_elements[0].text if floor_count_elements else None
+                balcony_value = balcony_elements[0].text if balcony_elements else None
+
+                if count == 2:
+                    print("Area: " + area_value)
+                    print("Room: " + room_value)
+                    print("Bathroom: " + bathroom_value)
+                    print("Floor Count: " + floor_count_value)
+                    print("Balcony Count: " + balcony_value)
+                count += 1
+
+                # 7 -
+                # elements = c.find_elements(by=By.CSS_SELECTOR, value="._2UELHn")
+                # price = c.find_elements(by=By.CSS_SELECTOR, value="._2C5UCT")
+                # location = c.find_elements(by=By.CSS_SELECTOR, value="._2wVG12")
+                # try:
+                #     for element in elements:
+                #         find_m2 = element.find_element(by=By.XPATH, value=".//span[contains(text(),'m2')]")
+                #         m2_text = find_m2.text
+                #         m2_value = re.search(r'\d+\s*m2', m2_text)
+                #         m2 = m2_value.group()
+                #         house_list.append({'price': int(price[0].text.replace('\n', '').replace('.', '').replace('TL', '')),
+                #                            'location': location[0].text.replace('\n', ''),
+                #                            'm2': m2.replace(' m2', '')})
+                # except:
+                #     print("Error")
         try:
             next_link = browser.find_element(by=By.XPATH, value="/html/body/div/div/div[3]/div[1]/div/div[7]/div["
                                                                 "1]/div[35]/div[1]/ul/li[3]/div")
