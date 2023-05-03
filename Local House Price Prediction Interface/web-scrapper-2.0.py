a01edbec80f89c25b41eba184735cec6c972b2b4
Furkan Öztürk <58707213+furkan15ozturk@users.noreply.github.com>
2023-05-03 16:56:00 +0300

    All necessary data is being collected.

--- Local House Price Prediction Interface/web-scrapper-2.0.py
+++ Local House Price Prediction Interface/web-scrapper-2.0.py
@@ -26,57 +26,61 @@ def update_houses():
             else:
                 house_link = c.find_element(by=By.CSS_SELECTOR, value="a")
                 house_href = house_link.get_attribute("href")
-                print(house_href)
+                # print(house_href)
                 browser2.get(house_href)
-                house_info = browser2.find_elements(by=By.CSS_SELECTOR, value="._35T4WV")
+                # house_info = browser2.find_elements(by=By.CSS_SELECTOR, value="._35T4WV")
 
-                # area_xpath = '//div[contains(text(), "Brüt Metrekare")]/following-sibling::div'
-                # room_xpath = '//div[contains(text(), "Oda Sayısı")]/following-sibling::div'
-                # bathroom_xpath = '//div[contains(text(), "Banyo Sayısı")]/following-sibling::div'
-                # floor_count_xpath = '//div[contains(text(), "Binanın Kat Sayısı")]/following-sibling::div'
-                # balcony_xpath = '//div[contains(text(), "Balkon Sayısı")]/following-sibling::div'
                 area_xpath = '//div[contains(text(), "Brüt Metrekare")]/following-sibling::div'
                 room_xpath = '//div[contains(text(), "Oda Sayısı")]/following-sibling::div'
                 floor_count_xpath = '//div[contains(text(), "Binanın Kat Sayısı")]/following-sibling::div'
                 building_age_xpath = '//div[contains(text(), "Binanın Yaşı")]/following-sibling::div'
+
                 area_elements = browser2.find_elements(by=By.XPATH, value=area_xpath)
                 room_elements = browser2.find_elements(by=By.XPATH, value=room_xpath)
                 floor_count_elements = browser2.find_elements(by=By.XPATH, value=floor_count_xpath)
                 building_age_elements = browser2.find_elements(by=By.XPATH, value=building_age_xpath)
+                price_element = browser2.find_elements(by=By.CSS_SELECTOR, value=".R-RKDB")
 
-                area_value = area_elements[0].text if area_elements else None
-                room_value = room_elements[0].text if room_elements else None
+                price = price_element[0].text.replace('.', '').replace('TL', '') if price_element else None
+                price_value = int(re.findall(r'\d+', price)[0])
+
+                area = area_elements[0].text if area_elements else None
+                area_value = int(area.replace(' M2', ''))
+
+                room = room_elements[0].text if room_elements else None
+                room_value = int(room[0]) + int(room[2])
+
                 floor_count_value = floor_count_elements[0].text if floor_count_elements else None
-                building_age_value = building_age_elements[0].text if building_age_elements else None
 
-                if count == 0:
-                    print("Area: " + area_value)
-                    print("Room: " + room_value)
-                    print("Floor Count: " + floor_count_value)
-                    print("Building age: " + building_age_value)
-                count += 1
+                building_age = building_age_elements[0].text.lower() if building_age_elements else None
+                if building_age == "0 (yeni)":
+                    building_age_value = 0
+                elif building_age == "5-10":
+                    building_age_value == 7.5
+                elif building_age == "11-15":
+                    building_age_value == 13.5
+                elif building_age == "16-20":
+                    building_age_value == 18.5
+                elif building_age == "21 ve üzeri":
+                    building_age_value == 21
+                else:
+                    building_age_value = int(building_age)
 
-                # 7 -
-                # elements = c.find_elements(by=By.CSS_SELECTOR, value="._2UELHn")
-                # price = c.find_elements(by=By.CSS_SELECTOR, value="._2C5UCT")
-                # location = c.find_elements(by=By.CSS_SELECTOR, value="._2wVG12")
-                # try:
-                #     for element in elements:
-                #         find_m2 = element.find_element(by=By.XPATH, value=".//span[contains(text(),'m2')]")
-                #         m2_text = find_m2.text
-                #         m2_value = re.search(r'\d+\s*m2', m2_text)
-                #         m2 = m2_value.group()
-                #         house_list.append({'price': int(price[0].text.replace('\n', '').replace('.', '').replace('TL', '')),
-                #                            'location': location[0].text.replace('\n', ''),
-                #                            'm2': m2.replace(' m2', '')})
-                # except:
-                #     print("Error")
+                house_list.append({'price': price_value,
+                                   'area': area_value,
+                                   'room': room_value,
+                                   'floor_count': floor_count_value,
+                                   'building_age': building_age_value})
+                #
+                # if count == 0:
+                #     print(house_list)
+                #     count += 1
         try:
             next_link = browser.find_element(by=By.XPATH, value="/html/body/div/div/div[3]/div[1]/div/div[7]/div["
                                                                 "1]/div[35]/div[1]/ul/li[3]/div")
             a = next_link.find_element(by=By.CSS_SELECTOR, value="a")
             link = a.get_attribute("href")
-            print(link)
+            # print(link)
             next_page = True
         except:
             next_page = False
