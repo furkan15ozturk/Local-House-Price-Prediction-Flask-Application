e032eaefbadd48b34b9056fe1f2e0a9217859d98
Furkan Öztürk <58707213+furkan15ozturk@users.noreply.github.com>
2023-05-03 19:58:20 +0300

    8 variables are scraped.

--- Local House Price Prediction Interface/web-scrapper-2.0.py
+++ Local House Price Prediction Interface/web-scrapper-2.0.py
@@ -31,26 +31,38 @@ def update_houses():
                 # house_info = browser2.find_elements(by=By.CSS_SELECTOR, value="._35T4WV")
 
                 area_xpath = '//div[contains(text(), "Brüt Metrekare")]/following-sibling::div'
+                absolute_area_xpath = '//div[contains(text(), "Net Metrekare")]/following-sibling::div'
                 room_xpath = '//div[contains(text(), "Oda Sayısı")]/following-sibling::div'
                 floor_count_xpath = '//div[contains(text(), "Binanın Kat Sayısı")]/following-sibling::div'
                 building_age_xpath = '//div[contains(text(), "Binanın Yaşı")]/following-sibling::div'
 
                 area_elements = browser2.find_elements(by=By.XPATH, value=area_xpath)
+                absolute_area_elements = browser2.find_elements(by=By.XPATH, value=absolute_area_xpath)
                 room_elements = browser2.find_elements(by=By.XPATH, value=room_xpath)
                 floor_count_elements = browser2.find_elements(by=By.XPATH, value=floor_count_xpath)
                 building_age_elements = browser2.find_elements(by=By.XPATH, value=building_age_xpath)
                 price_element = browser2.find_elements(by=By.CSS_SELECTOR, value=".R-RKDB")
+                location_element = browser2.find_elements(by=By.CSS_SELECTOR, value="._3VQ1JB")
 
                 price = price_element[0].text.replace('.', '').replace('TL', '') if price_element else None
                 price_value = int(re.findall(r'\d+', price)[0])
 
+                location = location_element[0].text if location_element else None
+                location_value = location.replace('location_on\n','')
+
                 area = area_elements[0].text if area_elements else None
                 area_value = int(area.replace(' M2', ''))
 
-                room = room_elements[0].text if room_elements else None
-                room_value = int(room[0]) + int(room[2])
+                absolute_area = absolute_area_elements[0].text if absolute_area_elements else None
+                absolute_area_value = int(absolute_area.replace(' M2', ''))
 
-                floor_count_value = floor_count_elements[0].text if floor_count_elements else None
+                room = room_elements[0].text if room_elements else None
+                room_count = re.findall(r'\d+', room)
+                if len(room_count) == 2:
+                    room_value = int(room_count[0]) + int(room_count[1])
+                else:
+                    room_value = int(room_count[0])
+                floor_count_value = int(floor_count_elements[0].text) if floor_count_elements else None
 
                 building_age = building_age_elements[0].text.lower() if building_age_elements else None
                 if building_age == "0 (yeni)":
@@ -66,11 +78,14 @@ def update_houses():
                 else:
                     building_age_value = int(building_age)
 
+
                 house_list.append({'price': price_value,
                                    'area': area_value,
+                                   'absolute_area': absolute_area_value,
                                    'room': room_value,
                                    'floor_count': floor_count_value,
-                                   'building_age': building_age_value})
+                                   'building_age': building_age_value,
+                                   'location': location_value})
                 #
                 # if count == 0:
                 #     print(house_list)
