4fdebcf9f0fab1e5f49c34869582ed822d7d7f41
Furkan Öztürk <58707213+furkan15ozturk@users.noreply.github.com>
2023-05-18 14:17:51 +0300

    Latitude and Longitude added to the csv file.

--- Local House Price Prediction Interface/latitude-longitude.py
+++ Local House Price Prediction Interface/latitude-longitude.py
@@ -1,9 +1,11 @@
 import googlemaps
 from datetime import datetime
+import unicodedata
+import pandas as pd
 
 gmaps = googlemaps.Client(key='AIzaSyCeoDaJO26EUzomxAwpDtbaBbEXUBSXpGk')
 
-geocode_result = gmaps.geocode('İstanbul - Adalar - Sivri Ada')
+geocode_result = gmaps.geocode('Istanbul - Çekmekoy - Güngoren Mahallesi')
 
 location = geocode_result[0]['geometry']['location']
 
@@ -12,3 +14,16 @@ lng = location['lng']
 
 print(f"Latitude: {lat}")
 print(f"Longitude: {lng}")
+
+
+def contains_non_latin(word):
+    for char in word:
+        if unicodedata.category(char)[0] != 'L':
+            return True
+    return False
+
+dataset = pd.read_csv('house_price_istanbul.csv', encoding='latin1')
+column_data = dataset['location']
+non_latin_words = [word for word in column_data if contains_non_latin(word)]
+print(non_latin_words)
+
---
+++ Local House Price Prediction Interface/location_to_lat_long.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import googlemaps
+import requests
+
+gmaps = googlemaps.Client(key='AIzaSyCeoDaJO26EUzomxAwpDtbaBbEXUBSXpGk')
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("house_price_istanbul.csv", encoding="latin1")
+
+    pd.set_option('display.max_columns', None)
+    count = 0
+    for index in df.index:
+        location = df.at[index, 'location']
+        gmaps = googlemaps.Client(key='AIzaSyCeoDaJO26EUzomxAwpDtbaBbEXUBSXpGk')
+        try:
+            geocode_result = gmaps.geocode(location)
+            lat = geocode_result[0]['geometry']['location']['lat']
+            lng = geocode_result[0]['geometry']['location']['lng']
+            df.at[index, 'lat'] = lat
+            df.at[index, 'lng'] = lng
+        except:
+            print(f"Hatalı String Değeri: {location} + Index: {index}")
+        # print(f"Latitude: {lat}")
+        # print(f"Longitude: {lng}")
+        count += 1
+        df_new = df.copy()
+
+
+    print(count)
+    df_new.to_csv( r'C:\Users\furka\OneDrive\Masaüstü\GPII Project\Local House Price Prediction Interface\CSV Files\house_price_istanbul_new.csv')
+
+
+
