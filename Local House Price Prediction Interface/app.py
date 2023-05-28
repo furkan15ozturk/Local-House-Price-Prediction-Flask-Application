6190512b8167ad5d289500ef042a281adaddb1e0
Furkan Öztürk <58707213+furkan15ozturk@users.noreply.github.com>
2023-05-28 07:43:47 +0300

    html and css files are updated

--- Local House Price Prediction Interface/app.py
+++ Local House Price Prediction Interface/app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import googlemaps
 
 app = Flask(__name__)
 
+gmaps = googlemaps.Client(key='AIzaSyCeoDaJO26EUzomxAwpDtbaBbEXUBSXpGk')
 
-@app.route('/')
+def get_lat_long(address):
+    geocode_result = gmaps.geocode(address)
+    lat = geocode_result[0]["geometry"]["location"]["lat"]
+    lng = geocode_result[0]["geometry"]["location"]["lng"]
+    return lat, lng
+@app.route('/', methods=['GET', 'POST'])
 def index():  # put application's code here
+    if request.method == 'POST':
+        area_value = int(request.form['area'])
+        absolute_area_value = int(request.form['absolute_area'])
+        room_value = int(request.form['room'])
+        floor_count_value = int(request.form['floor_count'])
+        building_age_value = int(request.form['building_age'])
+        lat_value, lng_value = get_lat_long(request.form['location'])
+        print("lat: ", lat_value, "lng: ", lng_value)
+        price_value = area_value * absolute_area_value * room_value * floor_count_value * building_age_value
+        return render_template("index.html", title="İstanbul Ev Fiyat Tahmin Sitesi", price=price_value)
     return render_template("index.html", title="İstanbul Ev Fiyat Tahmin Sitesi")
 
 
--- Local House Price Prediction Interface/latitude-longitude.py
+++ Local House Price Prediction Interface/latitude-longitude.py
@@ -15,12 +15,3 @@ lng = location['lng']
 print(f"Latitude: {lat}")
 print(f"Longitude: {lng}")
 
-
-dataset = pd.read_csv('CSV Files/house_price_istanbul_dataset.csv', encoding='latin1')
-dataset.drop('Unnamed: 0.1', axis=1, inplace=True)
-dataset.drop('Unnamed: 0', axis=1, inplace=True)
-dataset.drop('location', axis=1, inplace=True)
-# Print the column names
-column_names = dataset.columns
-print(column_names)
-dataset.to_csv( r'C:\Users\furka\OneDrive\Masaüstü\GPII Project\Local House Price Prediction Interface\CSV Files\house_price_istanbul.csv')
